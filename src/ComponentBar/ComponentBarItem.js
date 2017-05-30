2e144ad88a1fcebeec0dafc194c5db87e6880083
BJR Matos <bjrmatos@gmail.com>
2017-05-30 12:01:56 -0500

    generating invisible drag preview when dragging components from bar

--- src/ComponentBar/ComponentBarItem.js
+++ src/ComponentBar/ComponentBarItem.js
@@ -13,11 +13,55 @@ const componentSource = {
 function collect(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
+    connectDragPreview: connect.dragPreview(),
     isDragging: monitor.isDragging()
   }
 }
 
 class ComponentBarItem extends Component {
+  componentDidMount () {
+    /**
+     * Use empty element as a drag preview so browsers don't draw it
+     * and we can draw whatever we want on the custom drag layer instead.
+     * we could have used the `.getEmptyImage()` of react-dnd-html5-backend but
+     * IE does not support specifying images as the snapshot of a drag source so
+     * we use the cross-browser solution: an invisible element,
+     * we also use the `captureDraggingState` for our preview element to receive the
+     * isDragging state as fast as possible
+     */
+    this.props.connectDragPreview(this.getEmptyPreview(), {
+      captureDraggingState: true
+    });
+  }
+
+  getEmptyPreview (createElement) {
+    if (createElement === true) {
+      const {
+        isDragging,
+        component
+      } = this.props
+
+      /**
+       * drawing an invisible element for drag preview, the element is invisible at
+       * start because of absolute position an z-index: -1 (element is behind the content),
+       * while dragging the element is invisible because the opacity is set to 0.
+       * we need to apply different techniques at different stages to make the element invisible
+       * because some browsers (Safari) needs that the element be visible before being screenshotted,
+       * also the element needs to have some content (can't be empty or just filled with empty space)
+       */
+      return (
+        <div
+          ref={(el) => this.emptyPreview = el}
+          style={{ position: 'absolute', top: 0, left: 0, opacity: isDragging ? 0 : 1, zIndex: -1 }}
+        >
+          {`Preview-${component.name}`}
+        </div>
+      )
+    }
+
+    return this.emptyPreview
+  }
+
   render () {
     const {
       component,
@@ -29,6 +73,7 @@ class ComponentBarItem extends Component {
       <div className="ComponentBarItem" style={{ opacity: isDragging ? 0.7 : 1 }}>
         <span className={'ComponentBarItem-icon fa fa-' + component.icon} />
         <span className="ComponentBarItem-name">{component.name}</span>
+        {this.getEmptyPreview(true)}
       </div>
     )
   }
@@ -36,6 +81,7 @@ class ComponentBarItem extends Component {
 
 ComponentBarItem.propTypes = {
   connectDragSource: PropTypes.func.isRequired,
+  connectDragPreview: PropTypes.func.isRequired,
   isDragging: PropTypes.bool.isRequired
 };
 
