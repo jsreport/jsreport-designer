ed09a30ee3d7379dcff21fdce1152e70c3a569db
BJR Matos <bjrmatos@gmail.com>
2017-10-13 11:43:53 -0500

    rewire CRA build to allow decorators for MobX

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+
+const rewireForMobX = require('react-app-rewire-mobx');
+
+module.exports = (config, env) => {
+  // enabling decorators for mobx
+  config = rewireForMobX(config,env);
+
+  return config;
+}
