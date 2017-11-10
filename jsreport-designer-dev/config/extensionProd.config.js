8f6d3a6b4c57a948fa5569098e369ff7f3f86f66
BJR Matos <bjrmatos@gmail.com>
2017-11-10 16:31:18 -0500

    don’t allow assets loaders in extensions bundle

--- jsreport-designer-dev/config/extensionProd.config.js
+++ jsreport-designer-dev/config/extensionProd.config.js
@@ -96,29 +96,6 @@ module.exports = {
         test: /\.scss$/,
         include: [/.*theme.*\.scss/],
         use: getStylesConfig({ processor: 'sass' })
-      },
-      {
-        test: /\.less$/,
-        use: getStylesConfig({ modulesEnabled: true, processor: 'less' })
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'application/font-woff'
-        }
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      },
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: 'url-loader',
-        options: {
-          limit: 8192
-        }
       }
     ]
   },
